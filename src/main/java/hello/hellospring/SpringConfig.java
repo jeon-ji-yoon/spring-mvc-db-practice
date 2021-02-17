d4ecf2dd6e10eb12ee953de05409b166cf70e0da
jeon-ji-yoon <appleru1515@gmail.com>
2021-02-17 22:21:32 +0900

    2021-02-17 MemberController에 회원조회기능 추가하고 이를 thymeleaf템플릿 엔진으로 view를 구성 _ 그런데 회원 데이터는 컴퓨터의 메모리에 저장되므로 애플리케이션을 껐다 키면 data가 사라짐 _ db의 필요성을 알게됨

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -11,11 +11,13 @@ public class SpringConfig {
 
     @Bean //내가 스프링 빈을 등록할거야
     public MemberService memberService() {
+
         return new MemberService( memberRepository() );
     }
     //마치 Autowire처럼 스프링 빈에 등록된 객체를 파라미터로 넘겨준다
     @Bean
     public MemberRepository memberRepository() {
+
         return new MemoryMemberRepository();
     }
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,9 +4,12 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -33,5 +36,16 @@ public class MemberController {
         member.setName(form.getName());
 
         memberService.join(member);
+
+        return "redirect:/";
     }
+
+    @GetMapping("/members")
+    public String List(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); /*members라는 리스트
+        자체를 받아서 화면(view)에 넘긴다*/
+        return "members/memberList";
+    }
+
 }
